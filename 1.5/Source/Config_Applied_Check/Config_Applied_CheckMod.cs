7f3f98b2b9533034abca9baa6f9dd87bdd456599
Dexter Lowe <feldoh10@gmail.com>
2024-03-25 17:33:35 +0000

    1.5 Update

---
+++ 1.5/Source/Config_Applied_Check/Config_Applied_CheckMod.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace Config_Applied_Check;
+
+public class Config_Applied_CheckMod : Mod
+{
+    public static Settings settings;
+
+    public Config_Applied_CheckMod(ModContentPack content) : base(content)
+    {
+        // initialize settings
+        settings = GetSettings<Settings>();
+
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+
+        Harmony harmony = new("Taggerung.rimworld.Config_Applied_Check.main");
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Config Applied Check";
+    }
+}
